60a44ecfb5e051af2b4bf5358690f294a9fa5264
sudokzt <wato9902@gmail.com>
2021-11-20 18:34:08 +0900

    chore: initilize

---
+++ index.js
@@ -0,0 +1,7 @@
+var main = function() {
+    console.log("working2");
+};
+main();
+
+
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const main = () => {
+  console.log("working2");
+};
+
+main();
